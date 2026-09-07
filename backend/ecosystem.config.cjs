1a9e6c79a026a247abae65121c10235a47dc444f
cnktm <carlnktmensah@gmail.com>
2026-09-07 16:47:52 +0000

    5G bump

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -36,7 +36,7 @@ module.exports = {
       instances: 1,
       exec_mode: "fork",
       autorestart: true,
-      max_memory_restart: "4G",
+      max_memory_restart: "5G",
       kill_timeout: 15000,
       env_production: {
         NODE_ENV: "production",
